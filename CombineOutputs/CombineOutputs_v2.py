dac1d124c8dddca149b02c1e9d14de6e0a3c83d8
0emerald <uj21900@bristol.ac.uk>
2024-04-09 16:17:55 +0100

    CombineOutputs v2 there where not using pandas to write or read csv files

---
+++ CombineOutputs/CombineOutputs_v2.py
@@ -0,0 +1,54 @@
+import os
+import csv
+import sys
+
+# Increase the field size limit
+csv.field_size_limit(sys.maxsize)
+
+try:
+    c = int(sys.argv[1])
+except ValueError:
+    print("Error: The provided argument for c is not a valid integer.")
+    sys.exit(1)
+    
+try: 
+    n = int(sys.argv[2])
+except ValueError:
+    print("Error: The provided argument for n is not a valid integer.") 
+    sys.exit(1)
+    
+crawlDate = sys.argv[3]
+
+N = n // c  # integer division
+
+# Initialize output CSV file
+output_csv_path = "df" + crawlDate + ".csv"
+with open(output_csv_path, mode='w', newline='') as output_csv_file:
+    output_csv_writer = csv.writer(output_csv_file)
+    
+    idx = 0
+
+    for i in range(c):
+        folder_name = 'folder' + str(i)
+        folder_path = os.path.join(os.getcwd(), folder_name)
+        os.chdir(folder_path)
+
+        for file in range(N):
+            file_name = "crawldata" + crawlDate + "segment" + str(idx + file).zfill(5) + ".csv"
+            try:
+                with open(file_name, 'r') as csv_file:
+                    csv_reader = csv.reader(csv_file)
+                    # Check if the CSV file is empty
+                    is_empty = len(list(csv_reader)) == 0
+                    if is_empty:
+                        pass
+                    else: 
+                        for row in csv_reader:
+                            output_csv_writer.writerow(row)
+            except FileNotFoundError:
+                pass
+
+        parent_directory = os.path.dirname(os.getcwd())
+        os.chdir(parent_directory)
+        idx += N
+
